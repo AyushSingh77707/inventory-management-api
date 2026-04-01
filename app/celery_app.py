9c4d190937df9507e7313005058be8890141163f
Ayush <ayushsingh77707@gmail.com>
2026-04-01 12:24:29 +0530

    Added Docker & docker-compose setup

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,8 +1,11 @@
 from celery import Celery
+import os
+
+redis_url = os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0")
 
 celery_app=Celery(
-    "inventory_app",                          #celery app's name
-    broker="redis://localhost:6379/0",        #redis pe queue
-    backend="redis://localhost:6379/0",       #result redis me save hoga
-    include=["app.tasks"]                     #hmare task yaha hai
+    "inventory_app",          #celery app's name
+    broker=redis_url,        #redis pe queue
+    backend=redis_url,       #result redis me save hoga
+    include=["app.tasks"]     #hmare task yaha hai
 )
